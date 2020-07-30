e024ace23cfddc215e053f3de3ee76e169a99e6b
Dimuthu-abethunge <56988418+Dimuthu-abethunge@users.noreply.github.com>
2020-07-30 20:12:36 +0530

    Q1

---
+++ Q1.scala
@@ -0,0 +1,23 @@
+object RationalAssignment extends App{
+	val x = new Rational(3,4);
+	val y = new Rational(5,8);
+	val z = new Rational(2,7);
+
+	println("Value: "+x.neg);
+
+	val answer = (x.+(y.neg)).+(z.neg);
+	println("Substraction using negative value = " + answer);
+}
+
+class Rational(n:Int, d:Int){
+	def numer = n;
+	def denom = d;
+	
+	def neg = new Rational(-this.numer, this.denom);
+	
+	def +(r:Rational) = new Rational(this.numer*r.denom+ this.denom*r.numer, this.denom*r.denom);
+
+	def -(r:Rational) = this + r.neg;	
+
+	override def toString =  numer + "/" + denom;
+}
